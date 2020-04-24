5b577cf9e82e1c734be072107373cc365f3866aa
Anudeep Reddy <anudeepc85@gmail.com>
2020-04-24 05:41:21 +0000

    fixed testing logic

--- hello.js
+++ hello.js
@@ -1,13 +1,10 @@
 var spawn = require('child_process').spawn;
 function hello(callback){
-    var child = spawn('a.out');
+    var child = spawn('./a.out');
     child.stdin.end('12 34 56');
     child.stdout.on('data', (data) => {
         callback(data);
     });
-    child.on('close', (code) => console.log('Exit code: ' + code));
+    child.on('close', (code) => {});
 }
-hello((data)=>{
-    console.log("data");
-})
 module.exports = hello;
--- hello.test.js
+++ hello.test.js
@@ -2,7 +2,7 @@ const hello = require('./hello');
 
 test('Output Test', () => {
   function callback(data){
-    expect(data).toBe("12 34 56");
+    expect(data.toString()).toBe("12 34 56");
   }
   hello(callback);
 });
